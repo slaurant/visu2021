5ff63ebb7f37ce92d6b48d3fbd6464cbfeef2363
TheophileRobin <theo.r0709@gmail.com>
2021-11-29 17:07:01 +0100

    ajout du double slider

--- timeLine.js
+++ timeLine.js
@@ -1,16 +1,26 @@
-function filterBy(year) {
-	const filterYears = ['<=', ['number', ['get', 'year']], year];
-	
+function filterBy(year1, year2) {
+	const filterYears1 = ['>=', ['number', ['get', 'year']], year1];
+	const filterYears2 = ['<=', ['number', ['get', 'year']], year2];
 	for(const eventType of ["tsunamis", "earthquakes", "eruptions"]){
-		map.setFilter(eventType, filterYears)
-		// Set the label to the year
-	document.getElementById('year').textContent = year;
+		map.setFilter(eventType, filterYears1)
+		map.setFilter(eventType, filterYears2)
 	}}
   
-document.getElementById('timeline').addEventListener('change', (e) => {
-		console.log(e.target.value)
-		const timelineInput = parseInt(e.target.value, 10);
-		filterBy(timelineInput);
-		});
-//https://docs.mapbox.com/mapbox-gl-js/example/timeline-animation/?fbclid=IwAR3m_JXNJaiOBfc_XkXClvzY5cA62pgjnpLvPqCxO0_-JoHnnoTcwULaTZY
+
+$(document).ready(function(){
+    $( "#slider-range" ).slider({
+        range: true,
+        min: -2000,
+        max: 2021,
+        values: [ 75, 300 ],
+        slide: function( event, ui ) {
+          $( "#amount" ).text( "" + ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+		  filterBy(ui.values[ 0 ],ui.values[ 1 ]  );
+        }
+      });
+      $( "#amount" ).text( "" + $( "#slider-range" ).slider( "values", 0 ) +
+        " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+});
+
+
 
