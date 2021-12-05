a746bdcbd7747406ea8532db05ed761f4ac40a14
Simon Laurant <laurantsimon@gmail.com>
2021-12-05 12:56:08 +0100

    event Window

--- eventCard.js
+++ eventCard.js
@@ -1,50 +1,85 @@
-// Event Card
-// function: from a data point (event), render a div containing different elements about the event
-// informations: 
-//-- Title -- = event type + country 
-//-- Subtitle -- = date/period
+// modal window source : w3schools.com
 
-//informations: 
-// # deaths 
-// localisation
-// height
-// houses destroyed
-// comments
-// links? 
+// Get the modal
+var modal = document.getElementById("myModal");
 
 
-// provisoire: définir un data point
-var eventData = 
-{
-    "id": 12,
-    "year": -330,
-    "latitude": 40,
-    "longitude": 25,
-    "locationName": "E. SPORADES ISLANDS, AEGEAN ISLANDS",
-    "country": "GREECE",
-    "regionCode": 50,
-    "causeCode": 1,
-    "eventValidity": 1,
-    "comments": "330 B.C. Aegean Sea, Sporades Islands, 40 N 25 E, earthquake intensity XI, magnitude 7.0. It is said that the Chrysi Island together with the volcano of Moschylos disappeared as a result of an underground shock near the western shore of Limnos Island. Such a shock must have generated a strong tsunami. (reference 2130).\r\n<p>About 330, B.C. Eastern Sporades (Lemnos), 40 N, 25 E.\r\nIt is rumoured, that Chryse Island and the volcano Moschylos near the island of Lemnos were sunk by a shock. If it is true, a huge tsunami should have been generated. (reference #109)\r\n</p><p>330 B.C. Eastern Sporades Islands. Island of Chryse. Sieberg (1932a) p. 221 alludes to a seismic sea-wave to have accompanied the sinking of Chryse c. 330. No such event is mentioned by chroniclers of the time. Moreover, Chryse was afloat in 72 B.C. (reference #15)\r\n</p><p>330 B.C. Eastern Sporades Islands, 40 N 25 E. Island\r\nof Chryse. Earthquake intensity XI. (reference #20)\r\n</p><p>330 B.C. North Aegean Sea, 40 deg 06 min N 25 deg 12 min E, earthquake intensity XI. Sea disturbance in Chrysi Islet. (reference #2427)\r\n</p><p>330 B.C. Eastern Sporades, 40.0 N, 25.0 E, earthquake magnitude 7.0. (reference #330)</p>",
-    "earthquakeEventId": 22
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+
+
+function eventModal(eventId, eventType){
+  
+  // get Modal text
+  var modalText = d3.select("#modalText")
+  
+  //change modal content class for formatting
+  if(eventType  == "eruptions"){
+    var fileName = "Datasets_formatted/volcano_events_formatted.json"
+    var eventTypeText = "Eruption"
+    d3.select("#modalContent").style("background-color", "firebrick")
+  } else if(eventType == "earthquakes"){
+    var fileName = "Datasets_formatted/earthquakes_events_formatted.json"
+    var eventTypeText = "Earthquake"
+    d3.select("#modalContent").style("background-color", "seagreen")
+  } else if(eventType == "tsunamis"){
+    var fileName = "Datasets_formatted/tsunamis_events_formatted.json"
+    var eventTypeText = "Tsunami"
+    d3.select("#modalContent").style("background-color", "royalblue")
+  }
+
+ 
+
+  // get data from the right file
+  d3.json(fileName, function(data){  
+    // Clear modal window content
+    modalText.html("")
+
+    
+    // get data of the event
+    var eventData = data.filter(function(e){ return e.id == eventId})
+
+    console.log(eventData[0]["comments"])
+    // Content of Modal window
+
+
+    // Title
+    modalText.data(eventData).append("h1").text( function(e){ return eventTypeText + " in " + e["locationName"]} );
+    // Subtitle
+    modalText.data(eventData).append("h2").text(function(e){ return  e["year"]}).style("test-diplay", "center")
+
+    // Comments
+    modalText.data(eventData).append("p").html(function(e){ return  e["comments"]})
+    
+    modal.style.display = "block";
+  });
+   
+    
+
+  
+
+
+
+  
+};
+
+
+
+
+
+
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
 }
 
-
-
-var cardContent = document.getElementById("eventCard")
-
-var infos = ""
-
-// title
-infos += "<h1>tsunami in " + eventData.country.toLowerCase() +"</h1>"
-infos += "<h3> in "+ eventData.year + "</h3>"
-infos += eventData.comments
-
-
-cardContent.innerHTML = infos
-cardContent.className = "Element"
-
-
-
-
-console.log(infos)
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
