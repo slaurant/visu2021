6401f1d76ff0631fa3f82e923b20d05772b69fa8
TheophileRobin <theo.r0709@gmail.com>
2021-12-05 09:11:55 +0100

    ajout des nothing dans le top

--- deadliestEvents.js
+++ deadliestEvents.js
@@ -13,32 +13,65 @@ function selectTop3(min, max){
             if (data[i].year>=min && data[i].year<=max){
                 dataYears.push(data[i])
             }
-        }
-        
+        } 
         dataYears.sort(function (a, b) {
             return b.deaths - a.deaths;
             });
-        console.log(dataYears)
-        let locationGold = dataYears[0].locationName;
-        let locationSilver = dataYears[1].locationName;
-        let locationBronze = dataYears[2].locationName;
-        
-        let deathsGold = dataYears[0].deaths;
-        let deathsSilver = dataYears[1].deaths;
-        let deathsBronze = dataYears[2].deaths;
 
-        let typeGold = dataYears[0].typeEvent;
-        let typeSilver = dataYears[1].typeEvent;
-        let typeBronze = dataYears[2].typeEvent;
 
-        let yearGold = dataYears[0].year;
-        let yearSilver = dataYears[1].year;
-        let yearBronze = dataYears[2].year;
-
-        document.getElementById("firstDeadly").innerHTML ="[Year : " + yearGold + "] " + locationGold + " : " + deathsGold + " deaths (" + typeGold + ")";
-        document.getElementById("secondDeadly").innerHTML ="[" + yearSilver + "] " +  locationSilver + " : " + deathsSilver + " deaths (" + typeSilver + ")";
-        document.getElementById("thirdDeadly").innerHTML ="[" + yearBronze + "] " +  locationBronze + " : " + deathsBronze + " deaths (" + typeBronze + ")";
+        if (dataYears.length >= 3 ){
+            let locationGold = dataYears[0].locationName;
+            let locationSilver = dataYears[1].locationName;
+            let locationBronze = dataYears[2].locationName;
+            
+            let deathsGold = dataYears[0].deaths;
+            let deathsSilver = dataYears[1].deaths;
+            let deathsBronze = dataYears[2].deaths;
     
-    });
+            let typeGold = dataYears[0].typeEvent;
+            let typeSilver = dataYears[1].typeEvent;
+            let typeBronze = dataYears[2].typeEvent;
+    
+            let yearGold = dataYears[0].year;
+            let yearSilver = dataYears[1].year;
+            let yearBronze = dataYears[2].year;
+    
+            document.getElementById("firstDeadly").innerHTML ="[Year : " + yearGold + "] " + locationGold + " : " + deathsGold + " deaths (" + typeGold + ")";
+            document.getElementById("secondDeadly").innerHTML ="[" + yearSilver + "] " +  locationSilver + " : " + deathsSilver + " deaths (" + typeSilver + ")";
+            document.getElementById("thirdDeadly").innerHTML ="[" + yearBronze + "] " +  locationBronze + " : " + deathsBronze + " deaths (" + typeBronze + ")";
+        }
+        else if (dataYears.length === 2 ){
+            let locationGold = dataYears[0].locationName;
+            let locationSilver = dataYears[1].locationName;
+            
+            let deathsGold = dataYears[0].deaths;
+            let deathsSilver = dataYears[1].deaths;
+    
+            let typeGold = dataYears[0].typeEvent;
+            let typeSilver = dataYears[1].typeEvent;
+    
+            let yearGold = dataYears[0].year;
+            let yearSilver = dataYears[1].year;
+    
+            document.getElementById("firstDeadly").innerHTML ="[Year : " + yearGold + "] " + locationGold + " : " + deathsGold + " deaths (" + typeGold + ")";
+            document.getElementById("secondDeadly").innerHTML ="[" + yearSilver + "] " +  locationSilver + " : " + deathsSilver + " deaths (" + typeSilver + ")";
+            document.getElementById("thirdDeadly").innerHTML ="Nothing else";
+        }
+        else if (dataYears.length === 1){
+            let locationGold = dataYears[0].locationName;
+            let deathsGold = dataYears[0].deaths;
+            let typeGold = dataYears[0].typeEvent;
+            let yearGold = dataYears[0].year;
+    
+            document.getElementById("firstDeadly").innerHTML ="[Year : " + yearGold + "] " + locationGold + " : " + deathsGold + " deaths (" + typeGold + ")";
+            document.getElementById("secondDeadly").innerHTML ="Nothing else";
+            document.getElementById("thirdDeadly").innerHTML ="Nothing else";
+        }
+        else {
+            document.getElementById("firstDeadly").innerHTML ="Nothing";
+            document.getElementById("secondDeadly").innerHTML ="Nothing";
+            document.getElementById("thirdDeadly").innerHTML ="Nothing";
+        }
+   });
 }
 
--- eventCard.js
+++ eventCard.js
@@ -47,4 +47,3 @@ cardContent.className = "Element"
 
 
 
-console.log(infos)
