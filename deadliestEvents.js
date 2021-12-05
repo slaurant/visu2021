100453b063f40f92129c133a81b519443073ecf7
TheophileRobin <theo.r0709@gmail.com>
2021-12-05 12:53:24 +0100

    rendre le top plus beau

--- deadliestEvents.js
+++ deadliestEvents.js
@@ -7,6 +7,12 @@ function selectTop3(min, max){
     
     d3.json("Datasets_formatted/all_data.json", function(data){   // The code in the function is executed only when the data is loaded. All code requiring that the data is fully loaded shoud come here
         
+        const icon = {
+            "earthquake" : "fas fa-globe-americas", 
+            "tsunami" : "fas fa-water", 
+            "eruption" : "fas fa-fire-alt", 
+        }
+
         let dataYears = [];
         
         for (var i=0; i < data.length; i++){
@@ -35,10 +41,33 @@ function selectTop3(min, max){
             let yearGold = dataYears[0].year;
             let yearSilver = dataYears[1].year;
             let yearBronze = dataYears[2].year;
+
+            let iconTypeGold = icon[typeGold];
+            let iconTypeSilver =icon[typeSilver];
+            let iconTypeBronze = icon[typeBronze];
     
-            document.getElementById("firstDeadly").innerHTML ="[Year : " + yearGold + "] " + locationGold + " : " + deathsGold + " deaths (" + typeGold + ")";
-            document.getElementById("secondDeadly").innerHTML ="[" + yearSilver + "] " +  locationSilver + " : " + deathsSilver + " deaths (" + typeSilver + ")";
-            document.getElementById("thirdDeadly").innerHTML ="[" + yearBronze + "] " +  locationBronze + " : " + deathsBronze + " deaths (" + typeBronze + ")";
+            document.getElementById("dateGold").innerHTML = yearGold;
+            document.getElementById("locationGold").innerHTML = locationGold;
+            document.getElementById("deathsGold").innerHTML = deathsGold + " deaths";
+
+            document.getElementById("dateSilver").innerHTML = yearSilver;
+            document.getElementById("locationSilver").innerHTML = locationSilver;
+            document.getElementById("deathsSilver").innerHTML = deathsSilver + " deaths";
+
+            document.getElementById("dateBronze").innerHTML = yearBronze;
+            document.getElementById("locationBronze").innerHTML = locationBronze;
+            document.getElementById("deathsBronze").innerHTML = deathsBronze + " deaths";
+
+            document.getElementById("iconTypeGold").className = iconTypeGold ; 
+            document.getElementById("iconTypeSilver").className = iconTypeSilver ; 
+            document.getElementById("iconTypeBronze").className = iconTypeBronze ; 
+
+            document.getElementById("typeGold").innerHTML = typeGold;
+            document.getElementById("typeSilver").innerHTML = typeSilver;
+            document.getElementById("typeBronze").innerHTML = typeBronze;
+
+
+
         }
         else if (dataYears.length === 2 ){
             let locationGold = dataYears[0].locationName;
@@ -53,9 +82,25 @@ function selectTop3(min, max){
             let yearGold = dataYears[0].year;
             let yearSilver = dataYears[1].year;
     
-            document.getElementById("firstDeadly").innerHTML ="[Year : " + yearGold + "] " + locationGold + " : " + deathsGold + " deaths (" + typeGold + ")";
-            document.getElementById("secondDeadly").innerHTML ="[" + yearSilver + "] " +  locationSilver + " : " + deathsSilver + " deaths (" + typeSilver + ")";
-            document.getElementById("thirdDeadly").innerHTML ="Nothing else";
+            document.getElementById("dateGold").innerHTML = yearGold;
+            document.getElementById("locationGold").innerHTML = locationGold;
+            document.getElementById("deathsGold").innerHTML = deathsGold + " deaths";
+
+            document.getElementById("dateSilver").innerHTML = yearSilver;
+            document.getElementById("locationSilver").innerHTML = locationSilver;
+            document.getElementById("deathsSilver").innerHTML = deathsSilver + " deaths";
+
+            document.getElementById("dateBronze").innerHTML = "No other event";
+            document.getElementById("locationBronze").innerHTML = "No other event";
+            document.getElementById("deathsBronze").innerHTML = "No other event";
+
+            document.getElementById("iconTypeGold").className = iconTypeGold ; 
+            document.getElementById("iconTypeSilver").className = iconTypeSilver ; 
+            document.getElementById("iconTypeBronze").className = "fas fa-hand-middle-finger" ; 
+
+            document.getElementById("typeGold").innerHTML = typeGold;
+            document.getElementById("typeSilver").innerHTML = typeSilver;
+            document.getElementById("typeBronze").innerHTML = typeBronze;
         }
         else if (dataYears.length === 1){
             let locationGold = dataYears[0].locationName;
@@ -63,15 +108,34 @@ function selectTop3(min, max){
             let typeGold = dataYears[0].typeEvent;
             let yearGold = dataYears[0].year;
     
-            document.getElementById("firstDeadly").innerHTML ="[Year : " + yearGold + "] " + locationGold + " : " + deathsGold + " deaths (" + typeGold + ")";
-            document.getElementById("secondDeadly").innerHTML ="Nothing else";
-            document.getElementById("thirdDeadly").innerHTML ="Nothing else";
+            document.getElementById("dateGold").innerHTML = yearGold;
+            document.getElementById("locationGold").innerHTML = locationGold;
+            document.getElementById("deathsGold").innerHTML = deathsGold + " deaths";
+
+            document.getElementById("dateSilver").innerHTML = "No other event";
+            document.getElementById("locationSilver").innerHTML = "No other event";
+            document.getElementById("deathsSilver").innerHTML = "No other event";
+
+            document.getElementById("dateBronze").innerHTML = "No other event";
+            document.getElementById("locationBronze").innerHTML = "No other event";
+            document.getElementById("deathsBronze").innerHTML = "No other event";
+
+            document.getElementById("iconTypeGold").className = iconTypeGold ;  
+            document.getElementById("typeGold").innerHTML = typeGold;
+
         }
         else {
-            document.getElementById("firstDeadly").innerHTML ="Nothing";
-            document.getElementById("secondDeadly").innerHTML ="Nothing";
-            document.getElementById("thirdDeadly").innerHTML ="Nothing";
+            document.getElementById("dateGold").innerHTML = "No event";
+            document.getElementById("locationGold").innerHTML = "No event";
+            document.getElementById("deathsGold").innerHTML = "No event";
+
+            document.getElementById("dateSilver").innerHTML = "No event";
+            document.getElementById("locationSilver").innerHTML = "No event";
+            document.getElementById("deathsSilver").innerHTML = "No event";
+
+            document.getElementById("dateBronze").innerHTML = "No event";
+            document.getElementById("locationBronze").innerHTML = "No event";
+            document.getElementById("deathsBronze").innerHTML = "No event";
         }
    });
 }
-
