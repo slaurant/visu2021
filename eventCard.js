f0c3d2ea7dced5ce54ef6a33acb5ad3702fa4a35
Simon Laurant <laurantsimon@gmail.com>
2021-12-05 14:42:39 +0100

    pop up (2)

--- eventCard.js
+++ eventCard.js
@@ -1,5 +1,12 @@
 // modal window source : w3schools.com
 
+
+//get dictionnaries 
+var helper
+d3.json("Datasets/helper_dataset.json", function(data){
+  helper = data
+});
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -11,7 +18,7 @@ var span = document.getElementsByClassName("close")[0];
 
 
 function eventModal(eventId, eventType){
-  
+
   // get Modal text
   var modalText = d3.select("#modalText")
   
@@ -36,23 +43,60 @@ function eventModal(eventId, eventType){
   d3.json(fileName, function(data){  
     // Clear modal window content
     modalText.html("")
-
     
     // get data of the event
     var eventData = data.filter(function(e){ return e.id == eventId})
 
-    console.log(eventData[0]["comments"])
+
     // Content of Modal window
 
-
     // Title
-    modalText.data(eventData).append("h1").text( function(e){ return eventTypeText + " in " + e["locationName"]} );
-    // Subtitle
-    modalText.data(eventData).append("h2").text(function(e){ return  e["year"]}).style("test-diplay", "center")
+    modalText.data(eventData).append("h1").text( function(e){ return eventTypeText + " in " + e["country"]} );
+
+    // Subtitle: Date
+    var dateString
+    if(!!eventData[0]["startDate"]){
+      dateString = eventData[0]["startDate"]
+      if(!!eventData[0]["endDate"]){dateString += " to " +  eventData[0]["startDate"]}
+    } else{
+      dateString = eventData[0]["year"]
+      if(!!eventData[0]["month"]){
+        dateString+= "-"+('0' + eventData[0]["month"]).slice(-2)
+        if(!!eventData[0]["day"]){ dateString+="-"+('0' + eventData[0]["day"]).slice(-2) }
+      }
+    }
+    modalText.data(eventData).append("h2").text(dateString)
+
+    //Locations
+
+    modalText.append("h3").text("Location :")
+    //location name
+    if(!!eventData[0]["locationName"]){
+      modalText.data(eventData).append("p").text(function(e){return   e["locationName"] +  "   [" + e["latitude"] + ";"+ e["longitude"]+"]"})
+    };
+
+    // Consequences
+ 
+    modalText.append("h3").text("Consequences :")
+    // deaths
+    if(!!eventData[0]["deaths"]){
+      modalText.data(eventData).append("p").text(function(e){return  + e["deaths"] + " deaths"})
+    } 
+
+    if(!!eventData[0]["damageAmountOrder"]){
+      modalText.data(eventData).append("p").text(function(e){return "Damage Amount : " + helper["damage"][e["damageAmountOrder"]]["description"]})
+    } 
+
+
 
     // Comments
+    modalText.append("h3").text("Comments :")
+    // add Comments
     modalText.data(eventData).append("p").html(function(e){ return  e["comments"]})
     
+
+
+    //display modal window
     modal.style.display = "block";
   });
    
